b9fb9abf568a4b439bf1ad2ec46d364757efcf9c
vsenthilcbe <vsenthilcbe@gmail.com>
2024-07-31 18:13:00 -0400

    Initial Commit MyBlog

---
+++ src/test/java/com/springboot/blogapp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
